30f29cd8751652fcfb91ec9956222a6109dedf43
Musawar Ahmed <musawar.ahmed@kuonitumlare.com>
2022-09-12 12:28:47 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@
 */
 
 Route::get('/', function () {
-   /* try {
+    try {
        DB::connection()->getPdo();
     } catch (\Exception $e) {
         die("Could not connect to the database.  Please check your configuration. error:" . $e );
-    }*/
+    }
     return view('welcome');
 });
